ae78a221beeb7adbf827979cd54ece38945e3a86
Sebastian Juszczynski <sebastianjuszczynski@yahoo.com>
2020-07-24 23:23:18 +0100

    Changes in js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-const setDate = () => {
-    const secondHand = document.querySelector(".second-hand");
-    const minHand = document.querySelector(".min-hand");
-    const hourHand = document.querySelector(".hour-hand");
-    const now = new Date();
-    const seconds = now.getSeconds();
-    const minutes = now.getMinutes();
-    const hours = now.getHours();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
-    const minutesDegrees = ((minutes / 60) * 360) + 90;
-    const hoursDegrees = ((hours / 60) * 360) + 90;
-    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-    minHand.style.transform = `rotate(${minutesDegrees}deg)`;
-    hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+{
+    const setDate = () => {
+        const secondHand = document.querySelector(".second-hand");
+        const minHand = document.querySelector(".min-hand");
+        const hourHand = document.querySelector(".hour-hand");
+        const now = new Date();
+        const seconds = now.getSeconds();
+        const minutes = now.getMinutes();
+        const hours = now.getHours();
+        const secondsDegrees = ((seconds / 60) * 360) + 90;
+        const minutesDegrees = ((minutes / 60) * 360) + 90;
+        const hoursDegrees = ((hours / 12) * 360) + 90;
+        secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+        minHand.style.transform = `rotate(${minutesDegrees}deg)`;
+        hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    }
+    const init = () => {
+        setInterval(setDate, 1000);
+    }
+    init();
 }
-setInterval(setDate, 1000)
